3a02c64a6b8eaacb5e17bbab6b33a55f1adcab27
Ben van Staveren <benvanstaveren@gmail.com>
2015-07-08 20:01:12 +0700

    Adding vim modelines

--- lib/Keystone/Connect.pm
+++ lib/Keystone/Connect.pm
@@ -1,18 +1,9 @@
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 package Keystone::Connect;
 use Mojo::Base 'Mojolicious';
 
-# This method will run once at server start
 sub startup {
-  my $self = shift;
-
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
-
-  # Router
-  my $r = $self->routes;
-
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    my $self = shift;
 }
 
 1;
