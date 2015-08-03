948ba5248871beb6de216afa6b2341f20eb8b41f
Ben van Staveren <benvanstaveren@gmail.com>
2015-08-03 17:54:29 +0700

    let's see if this does it

--- lib/Keystone/Connect/Controller/Auth.pm
+++ lib/Keystone/Connect/Controller/Auth.pm
@@ -85,60 +85,170 @@ sub redirect_root {
     }
 }
 
+sub facebook_exchange_code_for_token {
+    my $self = shift;
+    my $code = shift;
+    my $cb   = shift;
+
+    $self->ua->get('https://graph.facebook.com/v2.4/oauth/access_token' => form => {
+        client_id       => $self->config('login')->{facebook}->{client_id},
+        redirect_uri    => sprintf('%s/auth/facebook_return/', $c->req->url->base),
+        client_secret   => $self->config('login')->{facebook}->{client_secret},
+        code            => $code
+    } => sub {
+        my ($ua, $tx) = (@_);
+
+        if(my $res = $tx->success) {
+            if(defined($res->json->{error})) {
+                return $cb->($self, $res->json->{error}, undef);
+            } else {
+                if(defined($res->json->{access_token})) {
+                    return $cb->($self, undef, $res->json->{access_token});
+                } else {
+                    return $cb->($self, { type => 'KCException', message => 'NO_ACCESS_TOKEN' }, undef);
+                }
+            }
+        } else {
+            return $cb->($self, { type => 'KCException', message => 'KC_REQ_ERROR' }, undef);
+        }
+    });
+}
+
+sub validate_auth_nonce {
+    my $self = shift;
+    my $nonce = shift;
+    my $cb = shift;
+
+    $self->db->query('SELECT id FROM auth_nonce WHERE nonce = ?', $nonce => sub {
+        my ($db, $err, $results) = (@_);
+
+        if(defined($err)) {
+            $self->redirect_root(status => 'error', 'error' => 'KC_NONCE_DB_ERROR');
+        } else {
+            if(my $nonce = $results->hash) {
+                if($nonce->{used} > 0) {
+                    $self->redirect_root(status => 'error', error => 'KC_NONCE_DUPLICATE');
+                } else {
+                    return $cb->($self);
+                }
+            } else {
+                $self->redirect_root(status => 'error', error => 'KC_NONCE_NOT_FOUND');
+            }
+        }
+    });
+}
+
+sub facebook_get_user_id_from_access_token {
+    my $self = shift;
+    my $token = shift;
+    my $cb = shift;
+
+    $self->ua->get('https://graph.facebook.com/debug_token' => form => {
+        input_token     => $token,
+        access_token    => $self->config('login')->{facebook}->{client_id},
+    } => sub {
+        my ($ua, $tx) = (@_);
+
+        if(my $res = $tx->success) {
+            if($res->json->{data}->{app_id} eq $self->config('login')->{facebook}->{client_id}) {
+                return $cb->($self, undef, $res->json->{data}->{user_id});
+            } else {
+                return $cb->($self, 'FB_INVALID_APP', undef);
+            }
+        } else {
+            return $cb->($self, 'KC_REQ_ERROR', undef);
+        }
+    });
+}
+
+sub facebook_get_user_profile {
+    my $self = shift;
+    my $uid  = shift;
+    my $token = shift;
+
+    $self->ua->get(sprintf('https://graph.facebook.com/v2.4/%s', $uid) => form => {
+        access_token => $token,
+        fields       => 'email,name',
+    } => sub {
+        my ($ua, $tx) = (@_);
+
+        if(my $res = $tx->success) {
+            # well, this should be fun! 
+            $self->render(json => $res->json);
+        } else {
+            return $cb->($self, 'FB_COULD_NOT_GET_PROFILE', undef);
+        }
+    });
+}
+
+sub sign_in_user {
+    my $self    = shift;
+    my $email   = shift;
+    my $cb      = shift;
+
+}
+
 sub facebook_return {
     my $self = shift;
 
     $self->render_later;
+    
+    if(my $error = $self->req->param('error')) {
+        if($error eq 'access_denied') {
+            $self->redirect_root(status => 'error', 'error' => 'FB_AUTH_DENIED_OR_CANCELLED');
+        } else {
+            $self->redirect_root(status => 'error', 'error' => 'FB_AUTH_ERROR');
+        }
+    } else {
+        if(my $code = $self->req->param('code')) {
+            if(my $nonce = $self->req->param('state')) {
+                $self->validate_auth_nonce($nonce => sub {
+                    my $self = shift; # only returns if the nonce is valid
 
-    if(defined($self->req->param('error'))) {
-        $self->stash(is_error => 1, error => $self->req->param('error'));
-        $self->redirect_root(status => 'error', error => 'FB_AUTH_ERROR');
-    } elsif(my $nonce = $self->req->param('#state')) {
-        $self->db->query('SELECT id FROM auth_nonce WHERE nonce = ?', $nonce => sub {
-            my ($db, $err, $results) = (@_);
+                    $self->facebook_exchange_code_for_token($code => sub {
+                        my ($self, $err, $token) = (@_);
 
-            if(my $nonce = $results->hash) {
-                if($nonce->{used} > 0) {
-                    $self->redirect_root(status => 'error', error => 'KC_NONCE_USED_ALREADY');
-                } elsif(my $token = $self->req->param('access_token')) {
-                    $self->ua->get('https://graph.facebook.com/debug_token' => form => {
-                        input_token     => $token,
-                        access_token    => $self->config('login')->{facebook}->{client_id},
-                    } => sub {
-                        my ($ua, $tx) = (@_);
+                        if(defined($err)) {
+                            # err is a hash, we just the message
+                            $self->redirect_root(status => 'error', error => $err->{message});
+                        } else {
+                            # hurray, we have an access token, go debug it
+                            $self->facebook_get_user_id_from_access_token($token => sub {
+                                my ($self, $err, $user_id) = (@_);
+        
+                                if(defined($err)) {
+                                    $self->redirect_root(status => 'error', error => $err);
+                                } else {
+                                    # grab the users' profile data
+                                    $self->facebook_get_user_profile($user_id => $token => sub {
+                                        my ($self, $err, $profile) = (@_);
 
-                        if(my $res = $tx->success) {
-                            if($res->json->{data}->{app_id} eq $self->config('login')->{facebook}->{client_id}) {
-                                my $user_id = $res->json->{data}->{user_id};
+                                        if(defined($err)) {
+                                            $self->redirect_root(status => 'error', error => $err);
+                                        } else {
+                                            # finally... 
+                                            $self->sign_in_user($profile->{email} => sub {
+                                                my ($self, $err, $kc_token) = (@_);
 
-                                # make a call to the graph API to get the user profile
-                                $self->ua->get(sprintf('https://graph.facebook.com/v2.4/%s', $user_id) => form => {
-                                    access_token => $token,
-                                } => sub {
-                                    my ($ua, $tx) = (@_);
-
-                                    if(my $res = $tx->success) {
-                                        # set up the user record in the database if we have one, if we don't then create it
-                                        # associate a token with the user record as well, 
-
-                                    } else {
-                                        $self->redirect_root(status => 'error', error => 'FB_COULD_NOT_GET_EMAIL');
-                                    }
-                                });
-                            } else {
-                                $self->redirect_root(status => 'error', 'error' => 'FB_INVALID_APP');
-                            }
+                                                if(defined($err)) {
+                                                    $self->redirect_root(status => 'error', error => $err);
+                                                } else {
+                                                    $self->redirect_root(status => 'ok', token => $kc_token);
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
                         }
                     });
-                } else {
-                    $self->redirect_root(status => 'error', error => 'FB_NO_TOKEN_RETURNED');
-                }
+                });
             } else {
-                $self->redirect_root(status => 'error', error => 'KC_NONCE_INVALID');
+                $self->redirect_root(status => 'error', 'error' => 'KC_NONCE_EMPTY');
             }
-        });
-    } else {
-        $self->redirect_root(status => 'error', error => 'KC_NONCE_EMPTY');
+        } else {
+            $self->redirect_root(status => 'error', 'error' => 'FB_NO_CODE');
+        }
     }
 }
 
